7fcd4b58d0e4500ef8351b46bc6facedde925cc2
CalebRuto <calebruto45@gmail.com>
2021-05-12 20:40:15 +0300

    Vanilla JS repo

---
+++ JS-Practice/variables.js
@@ -0,0 +1,8 @@
+let admin, name;
+
+name = 'John';  // Assigning a variable a value and transferring it to another variable
+admin = name;
+alert(admin);
+
+let ourPlanet = 'earth';  //Create a variable with our planets name
+let current_visitor = 'David'; //Create a variable for current visitor of a website
